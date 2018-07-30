724aa76e70dc35b00d8b699f4ba2709106a0e63a
Rogers F Silva <rogers.silva@datalyticsolutions.com>
2018-07-30 16:48:06 -0700

    assertions

--- scrip.py
+++ scrip.py
@@ -5,6 +5,9 @@ age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
 
+assert mean_age < 100
+assert mean_age > 10 
+
 np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
 print("done!")
